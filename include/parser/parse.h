54f906383e33d7375eb23910345891108e78dc86
emosaker <amywy@cock.li>
2025-06-07 14:54:45 +0200

    Typo

--- include/parser/parse.h
+++ include/parser/parse.h
@@ -2,11 +2,11 @@
  * This file is part of the leaf programming language
  */
 
-#ifndef LEAF_PARSER_H
-#define LEAF_PARSER_H
+#ifndef LEAF_PARSE_H
+#define LEAF_PARSE_H
 
 #include "parser/node.h"
 
 lfNode *lf_parse(const char *source, const char *file) ;
 
-#endif /* LEAF_PARSER_H */
+#endif /* LEAF_PARSE_H */
